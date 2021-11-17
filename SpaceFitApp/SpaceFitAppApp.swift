e78f07a0c14a4349a7138713a3bdaabdb8ef80be
Irene Fernando <irene87fernando@gmail.com>
2021-11-17 17:36:28 +0100

    FirstCommitTest

---
+++ SpaceFitApp/SpaceFitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceFitAppApp.swift
+//  SpaceFitApp
+//
+//  Created by Irene Fernando on 17/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SpaceFitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
