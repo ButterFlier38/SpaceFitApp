a43f2deebf585c76533594797ae2bdad8688adfb
Irene Fernando <irene87fernando@gmail.com>
2021-11-18 11:00:45 +0100

    Created the view files

---
+++ SpaceFitApp/DescriptionView.swift
@@ -0,0 +1,20 @@
+//
+//  DescriptionView.swift
+//  SpaceFitApp
+//
+//  Created by Irene Fernando on 18/11/21.
+//
+
+import SwiftUI
+
+struct DescriptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DescriptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        DescriptionView()
+    }
+}
---
+++ SpaceFitApp/TutorialView.swift
@@ -0,0 +1,20 @@
+//
+//  TutorialView.swift
+//  SpaceFitApp
+//
+//  Created by Irene Fernando on 18/11/21.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TutorialView_Previews: PreviewProvider {
+    static var previews: some View {
+        TutorialView()
+    }
+}
