3c859ce6ffc4e118421b9b23d4ba092455294901
Andrea <giovanni.ga50@gmail.com>
2021-11-18 11:55:30 +0100

    Progress bar without right value

--- SpaceFitApp/ContentView.swift
+++ SpaceFitApp/ContentView.swift
@@ -8,16 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("thh")
-        Text("SpaceFit")
-        Text("Hello")
-        Text("thh")
-
-            .padding()
+    @State private var progressValue: Float = 0.0
+    
+    var body: some View{
+        ZStack{
+            Color.white
+                .opacity(0.1)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                MyProgressBar(progressValue: $progressValue)
+                    .frame(width: 150.0, height: 150.0)
+                    .padding(30.0)
+                Spacer()
+                
+            }
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ SpaceFitApp/ProgressBar.swift
@@ -0,0 +1,26 @@
+//
+//  MyProgressBar.swift
+//  Space fit
+//
+//  Created by Andrea Autiero on 17/11/21.
+//
+
+import SwiftUI
+
+struct MyProgressBar: View {
+    @Binding var progressValue: Float
+    var body: some View{
+        ZStack{
+        Circle()
+            .stroke(lineWidth: 15.0)
+            .opacity(0.3)
+            .foregroundColor(Color.red)
+            Circle()
+                .trim(from: 0.0, to:CGFloat(min(self.progressValue,1.0)))
+                .stroke(style: StrokeStyle(lineWidth: 20.0, lineCap: .round, lineJoin: .round))
+                .foregroundColor(Color.red)
+                .rotationEffect(Angle(degrees: 270.0))
+                .animation(.linear)
+    }
+}
+}
