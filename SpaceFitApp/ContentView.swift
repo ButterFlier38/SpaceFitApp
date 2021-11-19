6c5f31533d39d4d7e471334eeb5a7118825afbb9
Irene Fernando <irene87fernando@gmail.com>
2021-11-19 10:41:24 +0100

    Connected ProgressBar and the Exercises

--- SpaceFitApp/ContentView.swift
+++ SpaceFitApp/ContentView.swift
@@ -5,16 +5,256 @@
 //  Created by Irene Fernando on 17/11/21.
 //
 
+
+
 import SwiftUI
 
+
 struct ContentView: View {
-    @State private var progressValue: Float = 0.0
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1)
+        UINavigationBar.appearance().backgroundColor = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
+        }
     
-    var body: some View{
-        ZStack{
-            Color.white
-                .opacity(0.1)
-                .edgesIgnoringSafeArea(.all)
+    
+    var body: some View {
+        TabView {
+            AerobicPage()
+                .tabItem{
+                        Image(systemName: "heart.fill")
+                        Text("Aerobic")
+                }
+            PowerPage()
+                .tabItem{
+                        Image(systemName: "flame.fill")
+                        Text("Power")
+                }
+            CorePage()
+                .tabItem{
+                        Image(systemName: "square.grid.3x3.middle.filled")
+                        Text("Core")
+                }
+        }
+    }
+}
+
+let PageColor : Color = Color(red: 255/255, green: 255/255, blue: 255/255)
+
+struct AerobicPage: View {
+    
+    @State private var progressValue: Float = 0.3
+    var body: some View {
+        NavigationView {
+            ZStack{
+                Color.white
+                    .opacity(0.1)
+                    .edgesIgnoringSafeArea(.all)
+               
+            
+        HStack {
+            VStack {
+                Spacer()
+                HStack{
+                Image(uiImage: UIImage(named: "CircleProgress.png")!)
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    .padding()
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )
+                }
+                .padding()
+                
+                HStack{
+                Image(uiImage: UIImage(named: "CircleProgress.png")!)
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    .padding()
+//                    .border(Color.black, width: 2)
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )
+                }
+                .padding()
+            }
+            Spacer()
+            VStack {
+                Spacer()
+                HStack{
+                Image(uiImage: UIImage(named: "CircleProgress.png")!)
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    .padding()
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )                }
+                .padding()
+
+                HStack{
+                Image(uiImage: UIImage(named: "CircleProgress.png")!)
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    .padding()
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )                }
+                .padding()
+
+            }
+        }.background(PageColor)
+                .navigationBarTitle("Aerobic")
+            
+                VStack{
+                    MyProgressBar(progressValue: $progressValue)
+                        .frame(width: 150.0, height: 150.0)
+                        .padding(30.0)
+                    Spacer()
+                    
+                }
+            }
+     }
+    }
+}
+
+struct PowerPage: View {
+    @State private var progressValue: Float = 0.0
+    var body: some View {
+        NavigationView {
+            ZStack{
+                Color.white
+                    .opacity(0.1)
+                    .edgesIgnoringSafeArea(.all)
+        HStack {
+            VStack {
+                Spacer()
+                HStack{
+                Image(uiImage: UIImage(named: "Earth.png")!)
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    .padding()
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )                }
+                .padding()
+                
+                HStack{
+                Image(uiImage: UIImage(named: "CircleProgress.png")!)
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    .padding()
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )                }
+                .padding()
+            }
+            Spacer()
+            VStack {
+                Spacer()
+                HStack{
+                Image(uiImage: UIImage(named: "CircleProgress.png")!)
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    .padding()
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )                }
+                .padding()
+
+                HStack{
+                Image(uiImage: UIImage(named: "CircleProgress.png")!)
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    .padding()
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )                }
+                .padding()
+
+            }
+        }.background(PageColor)
+         .navigationBarTitle("Power")
+                VStack{
+                    MyProgressBar(progressValue: $progressValue)
+                        .frame(width: 150.0, height: 150.0)
+                        .padding(30.0)
+                    Spacer()
+                    
+                }
+            }
+     }
+    }
+}
+
+struct CorePage: View {
+    @State private var progressValue: Float = 0.0
+    var body: some View {
+        NavigationView {
+            ZStack{
+                Color.white
+                    .opacity(0.1)
+                    .edgesIgnoringSafeArea(.all)
+        HStack {
+            VStack {
+                Spacer()
+                HStack{
+                Image(uiImage: UIImage(named: "CircleProgress.png")!)
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    .padding()
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )                }
+                .padding()
+                
+                HStack{
+                Image(uiImage: UIImage(named: "CircleProgress.png")!)
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    .padding()
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )                }
+                .padding()
+            }
+            Spacer()
+            VStack {
+                Spacer()
+                HStack{
+                Image(uiImage: UIImage(named: "CircleProgress.png")!)
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    .padding()
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )                }
+                .padding()
+
+                HStack{
+                Image(uiImage: UIImage(named: "CircleProgress.png")!)
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    .padding()
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )                }
+                .padding()
+                
+            }
+        }.background(PageColor)
+         .navigationBarTitle("Core")
+            
             VStack{
                 MyProgressBar(progressValue: $progressValue)
                     .frame(width: 150.0, height: 150.0)
@@ -22,11 +262,7 @@ struct ContentView: View {
                 Spacer()
                 
             }
-        }
-    }
-}
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+            }
+     }
     }
 }
